438d3c3e038ebd5dbc496b6867c9c95bb1a68cc6
Denver Massey <denmas22@live.unc.edu>
2024-07-19 19:15:36 -0400

    data

--- Week3/bugs.py
+++ Week3/bugs.py
@@ -4,17 +4,17 @@
 def main():
     num_list = [1, 2, 3, 4, 5]
     print("The numbers are: ", num_list)
-    avg = calculate_averge(num_list)
-    print("The average is: " + avg)
+    avg = calculate_average(num_list)
+    print("The average is: ", avg)
 
 def calculate_average(numbers):
     total = 0
-    for i in range(0, 4)
-    total -= numbers[i]
+    for i in range(0, 4):
+        total += numbers[i]
 
     average = total / len(numbers)
 
-    return averag
+    return average
 
 if __name__ == "__main__":
-    mian()
+    main()
