7e28c1c82ba7b72c1999baa48361d7515b48ab3b
Denver Massey <denmas22@live.unc.edu>
2024-07-19 16:47:09 -0400

    setup

---
+++ Week3/bugs.py
@@ -0,0 +1,20 @@
+# This program has a few bugs
+# It should calculate the average of the numbers 1, 2, 3, 4, and 5...
+
+def main():
+    num_list = [1, 2, 3, 4, 5]
+    print("The numbers are: ", num_list)
+    avg = calculate_averge(num_list)
+    print("The average is: " + avg)
+
+def calculate_average(numbers):
+    total = 0
+    for i in range(0, 4)
+    total -= numbers[i]
+
+    average = total / len(numbers)
+
+    return averag
+
+if __name__ == "__main__":
+    mian()
